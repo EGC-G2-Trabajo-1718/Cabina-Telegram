9bd291aae66e115500e0f3ed21dcf5af5ff67571
Víctor Rosa <vicrosa25@gmail.com>
2018-01-14 14:33:26 +0100

    Se incluye test a la clase ExitFunctionality.java

---
+++ basicBot/src/test/java/functionality/test/ExitFunctionalityTest.java
@@ -0,0 +1,25 @@
+package functionality.test;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Test;
+import functionality.ExitFunctionality;
+
+
+
+
+
+public class ExitFunctionalityTest {
+
+	
+	@Test
+	public void exitTest(){
+		ArrayList<String> usuarios = new ArrayList<String>();
+		usuarios.add("Victor");
+		ExitFunctionality.exit(usuarios, "Victor");
+		
+		assertTrue(!usuarios.contains("Victor"));
+	}
+	
+	
+}
